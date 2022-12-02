4f1fafde4e04b0128ea9f67df0099859efda55f2
Geshata <64840882+g3shaa@users.noreply.github.com>
2022-12-02 11:27:15 +0200

    Delete RandomGeneratorOfNumsConsole.js

--- RandomGeneratorOfNumsConsole.js
+++
@@ -1,8 +0,0 @@
-function numbers(){
-  let min = 1;
-  let max = 25;
-  let a = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log("Случайната стойност на " + min + " и " + max + " е " + a);
-}
-
-numbers()
